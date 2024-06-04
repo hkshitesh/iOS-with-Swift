bf239b58f3129d3df3c06ef10ec47c3e52c4707f
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-06-04 22:18:02 +0530

    Add files via upload

---
+++ DemoTableView/DemoTableView/Data.swift
@@ -0,0 +1,13 @@
+//
+//  Data.swift
+//  DemoTableView
+//
+//  Created by Hitesh Sharma - Vendor on 04/06/24.
+//
+
+import Foundation
+
+struct TableData{
+    let sectionType: String?
+    let sectionName: [String?]
+}
