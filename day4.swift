240576dc544eecc3676e96d5507c0c55c0330ea1
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-06-06 21:34:44 +0530

    Update day4.swift

--- day4.swift
+++ day4.swift
@@ -33,3 +33,42 @@ class ViewController: UIViewController {
         print("viewDidDisappear")
     }
 }
+
+
+
+
+
+//WebView:
+
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    var webView: WKWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(webView)
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        //Load URL
+        if let url = URL(string: "https://www.google.com"){
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+
+
+}
+
