334bd9cc45d666153fda3ddba48013665aad125c
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-06-06 21:03:52 +0530

    Update day4.swift

--- day4.swift
+++ day4.swift
@@ -1,1 +1,35 @@
 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        print("loadView -- 1")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad -- 1")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear -- 1")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear -- 1")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear -- 1")
+    }
+   
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+}
