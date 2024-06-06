7ba761caad573a86d2f01f1df2aab64c3c9ccc45
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-06-06 23:38:31 +0530

    Update day4.swift

--- day4.swift
+++ day4.swift
@@ -218,3 +218,24 @@ class ViewController: UIViewController {
 "hello" = "¡Hola, Mundo!";
 "button_title" = "Presióname";
 
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set localized strings
+        label.text = NSLocalizedString("hello", comment: "Hello, World!")
+        button.setTitle(NSLocalizedString("button_title", comment: "Press Me"), for: .normal)
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // Change label text when button is pressed
+        label.text = NSLocalizedString("button_title", comment: "Button Pressed")
+    }
+}
